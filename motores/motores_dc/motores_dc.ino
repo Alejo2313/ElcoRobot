3b92e8b65966eb051e854938b2b174e9ef411c8c
Joterom <oterosaxo@gmail.com>
2018-04-17 12:45:04 +0200

    2.0

--- motores/motores_dc/motores_dc.ino
+++ motores/motores_dc/motores_dc.ino
@@ -7,26 +7,68 @@
  ELECTRONILAB.CO
  */
 
-int IN3 = 5;    // Input3 conectada al pin 5
-int IN4 = 4;    // Input4 conectada al pin 4 
-int ENB = 3;    // ENB conectada al pin 3 de Arduino
-void setup()
-{
- pinMode (ENB, OUTPUT); 
- pinMode (IN3, OUTPUT);
- pinMode (IN4, OUTPUT);
+ /* 
+  *  Javier Otero Martinez
+  *  Miguel Garcia Maya
+  *  ELCO - 2018
+  *  
+  */
+
+#define IZQ_GIR1 7   //Verde
+#define IZQ_GIR0 6   //Amarillo
+#define DER_GIR1 4   //Naranja
+#define DER_GIR0 5   //Rojo
+#define DATA_PIN 10
+#define PWM_IZQ  8   //Gris
+#define PWM_DER  3   //Blanco 
+
+#define VELOCIDAD_FULL 230
+
+void setup() {
+ pinMode (PWM_IZQ, OUTPUT); 
+ pinMode (PWM_DER, OUTPUT);
+ pinMode (IZQ_GIR0, OUTPUT);
+ pinMode (IZQ_GIR1, OUTPUT);
+ pinMode (DER_GIR0, OUTPUT);
+ pinMode (DER_GIR1, OUTPUT);
+ pinMode (DATA_PIN, INPUT);
 }
-void loop()
-{
-  //Preparamos la salida para que el motor gire en un sentido
-  digitalWrite (IN3, HIGH);
-  digitalWrite (IN4, LOW);
-  // Aplicamos PWM al pin ENB, haciendo girar el motor
-  analogWrite(ENB,255);
 
-  delay(1000);
-  digitalWrite (IN4, HIGH);
-  digitalWrite (IN3, LOW);
-
+void loop() {
+  avanza();
   delay(2000);
+  para();
+  delay(2000);
+  atras();
+  delay(2000);
+  para();
+  while (1) {delay(1000);}
 }
+
+void avanza(){
+ digitalWrite (IZQ_GIR0, HIGH);
+ digitalWrite (IZQ_GIR1, LOW);
+ digitalWrite (DER_GIR0, HIGH);
+ digitalWrite (DER_GIR1, LOW);
+ analogWrite (PWM_IZQ, VELOCIDAD_FULL);
+ analogWrite (PWM_DER, VELOCIDAD_FULL);
+}
+
+void para(){
+ analogWrite (PWM_IZQ, 0);
+ analogWrite (PWM_DER, 0); 
+}
+
+void atras() {
+ digitalWrite (IZQ_GIR0, LOW);
+ digitalWrite (IZQ_GIR1, HIGH);
+ digitalWrite (DER_GIR0, LOW);
+ digitalWrite (DER_GIR1, HIGH);
+ analogWrite (PWM_IZQ, VELOCIDAD_FULL);
+ analogWrite (PWM_DER, VELOCIDAD_FULL);
+}
+
+
+
+
+
